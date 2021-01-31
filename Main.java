ca24ff71f7edebde8120a8d944913d8238e817ed
john-reiner <johnreiner1990@gmail.com>
2021-01-31 10:42:52 -0500

    reference values

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 package com.johnreiner;
 
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        byte age = 30;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = false;
-	    System.out.println(herAge);
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
     }
 }
