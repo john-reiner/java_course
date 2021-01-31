d98d1f8cdb82b218961227d2b7cb19e7544a11f5
john-reiner <johnreiner1990@gmail.com>
2021-01-31 10:30:49 -0500

    Java types

--- Main.java
+++ Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         byte age = 30;
-	    System.out.println(age);
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+	    System.out.println(herAge);
     }
 }
