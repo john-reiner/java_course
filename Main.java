47c9dfc68fb2418efc271ad9a260e4a69d698696
john-reiner <johnreiner1990@gmail.com>
2021-01-31 10:21:03 -0500

    integer types number

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package com.johnreiner;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello World");
+        byte age = 30;
+	    System.out.println(age);
     }
 }
